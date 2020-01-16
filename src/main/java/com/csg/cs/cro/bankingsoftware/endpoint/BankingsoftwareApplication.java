512d4f8b165eb857f4d9a502a557526ef4b21f8a
Jonas Gredig <jongredig@gmail.com>
2020-01-16 11:35:36 +0100

    add webfilter for CORS Policies

--- src/main/java/com/csg/cs/cro/bankingsoftware/endpoint/BankingsoftwareApplication.java
+++ src/main/java/com/csg/cs/cro/bankingsoftware/endpoint/BankingsoftwareApplication.java
@@ -1,7 +1,10 @@
 package com.csg.cs.cro.bankingsoftware.endpoint;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.core.Application;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
